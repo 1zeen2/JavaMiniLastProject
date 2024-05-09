46319dde47d911341a0e5c1c967ac04353527be4
user <user@DESKTOP-B49LN3Q>
2024-05-09 18:16:19 +0900

    클래스 문제와 정답, 클래스 구성 요소_변수 / 메서드1, 2

---
+++ Chapter05_클래스구성요소/src/com/sist/main/클래스구성요소_메서드2.java
@@ -0,0 +1,18 @@
+package com.sist.main;
+
+import java.util.*;
+
+class Diary2 {
+	private int yaer;
+	private int month;
+	private int week;
+	private int lastwwo;
+}
+public class 클래스구성요소_메서드2 {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
